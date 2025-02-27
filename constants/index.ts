c4ac2ba56f5a1a837d431e7e471a36fa46a20793
Chofor Cliford <betterfuture032@gmail.com>
2025-02-26 23:01:59 -0500

    remove App.css, update App component styling, and add new SVG assets

---
+++ constants/index.ts
@@ -0,0 +1,97 @@
+export const navLinks = [
+  {
+    title: "Music",
+    href: "/music",
+  },
+  {
+    title: "Live",
+    href: "/live",
+  },
+  {
+    title: "Podcast",
+    href: "/podcast",
+  },
+];
+
+export const sidebarLinks = [
+  {
+    category: "Menu",
+    links: [
+      {
+        title: "Home",
+        href: "/",
+        icon: "/assets/home-fill.svg",
+      },
+      {
+        title: "Explore",
+        href: "/explore",
+        icon: "/assets/explore.svg",
+      },
+      {
+        title: "Albums",
+        href: "/albums",
+        icon: "/assets/album.svg",
+      },
+      {
+        title: "Artists",
+        href: "/artists",
+        icon: "/assets/person.svg",
+      },
+      {
+        title: "Videos",
+        href: "/videos",
+        icon: "/assets/video.svg",
+      },
+    ],
+  },
+  {
+    category: "Library",
+    links: [
+      {
+        title: "Recent",
+        icon: "/assets/recent.svg",
+      },
+      {
+        title: "Albums",
+        icon: "/assets/album.svg",
+      },
+      {
+        title: "Podcasts",
+        icon: "/assets/podcasts.svg",
+      },
+      {
+        title: "Downloads",
+        icon: "/assets/downloads.svg",
+      },
+      {
+        title: "Locals",
+        icon: "/assets/loacals.svg",
+      },
+    ],
+  },
+  {
+    category: "Playlists",
+    links: [
+      {
+        title: "Create New Playlist",
+        icon: "/assets/arrow-up.svg",
+      },
+      {
+        title: "American Pop",
+        icon: "/assets/arrow-up.svg",
+      },
+      {
+        title: "American Hitz",
+        icon: "/assets/arrow-up.svg",
+      },
+      {
+        title: "Nigerian Hitz",
+        icon: "/assets/arrow-up.svg",
+      },
+      {
+        title: "African",
+        icon: "/assets/arrow-up.svg",
+      },
+    ],
+  },
+];
